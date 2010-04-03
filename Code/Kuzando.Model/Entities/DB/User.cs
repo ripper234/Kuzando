b4f494577a373017813a61789e300a1c1747f939
unknown <ron@.(none)>
2010-04-03 14:47:29 +0300

    Add database

--- Code/Kuzando.Model/Entities/DB/User.cs
+++ Code/Kuzando.Model/Entities/DB/User.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Castle.ActiveRecord;
 
 namespace Kuzando.Model.Entities.DB
 {
+    [ActiveRecord]
     public class User
     {
+        [PrimaryKey]
+        public virtual int Id { get; set; }
+
+        [Property]
+        public virtual string Name { get; set; }
+
+        [HasMany]
+        public virtual IList<Task> Tasks{ get; set; }
+
+        [Property]
+        public DateTime SignupDate { get; set; }
+
+        [Property]
+        public string OpenId { get; set; }
+
+        [Property]
+        public string Email { get; set; }
     }
 }
---
+++ Database/all.sql
@@ -0,0 +1,48 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.02 
+MySQL - 5.1.42-community : Database - kuzando
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`kuzando` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `kuzando`;
+
+/*Table structure for table `tasks` */
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `Id` int(4) unsigned NOT NULL AUTO_INCREMENT,
+  `UserId` int(4) unsigned NOT NULL,
+  `Title` varchar(100) NOT NULL,
+  `Body` varchar(5000) NOT NULL,
+  `CreatedDate` datetime NOT NULL,
+  `DueDate` datetime DEFAULT NULL,
+  `IsDone` tinyint(1) NOT NULL,
+  `Importance` tinyint(4) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `Id` int(4) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(30) NOT NULL,
+  `SignupDate` datetime NOT NULL,
+  `OpenId` varchar(100) NOT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
